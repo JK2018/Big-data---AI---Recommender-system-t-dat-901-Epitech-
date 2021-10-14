25ccf4a3fb29b0f4dda58d92b5a85f79cfbe99f7
Ryan Headley <ryan.headley@quant.global>
2021-10-14 16:04:20 +0200

    adding blank functions in integrate files

---
+++ general.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import pandas as pd
+
+
+def get_nb_clients():
+    pass
+
+def get_avg_product_price():
+    pass
+
+def get_total_sales():
+    pass
+
+def get_nb_unique_libelle():
+    pass
+
+def get_nb_unique_maille():
+    pass
+
+def get_nb_unique_univers():
+    pass
+
+def get_nb_unique_famille():
+    pass
+
+def get_mean_product_price_per_maille():
+    pass
+
+def get_mean_product_price_per_famille():
+    pass
+
+def get_mean_product_price_per_univers():
+    pass
+
+def get_nb_unique_libelle_per_maille():
+    pass
+
+def get_nb_unique_libelle_per_famille():
+    pass
+
+def get_nb_unique_libelle_per_univers():
+    pass
+
+def get_standard_deviation_nb_products_per_client():
+    pass
+
+def get_avg_nb_products_per_client():
+    pass
+
+def get_avg_total_expenses_per_client():
+    pass
+
+def get_avg_ticket_price():
+    pass
+
+def get_avg_nb_product_per_ticket():
+    pass
+
+def get_all_products():
+    pass
---
+++ user.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import pandas as pd
+
+
+def get_user_id():
+    pass
+
+def get_user_ticket_ids():
+    pass
+
+def get_total_nb_carts():
+    pass
+
+def get_total_expenses():
+    pass
+
+def get_most_expensive_cart():
+    pass
+
+def get_least_expensive_cart():
+    pass
+
+def get_avg_cart_price():
+    pass
+
+def get_most_expensive_product():
+    pass
+
+def get_all_expenses_per_month():
+    pass
+
+def get_nb_carts_per_month():
+    pass
+
+def get_purchase_freq_per_product():
+    pass
+
+def get_purchase_freq_per_maille():
+    pass
+
+def get_purchase_freq_per_univers():
+    pass
+
+def get_purchase_freq_per_famille():
+    pass
+
