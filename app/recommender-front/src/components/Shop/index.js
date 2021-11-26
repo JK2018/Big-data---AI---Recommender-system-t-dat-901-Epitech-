9aa7f1165cd2496242eecbde40e6aae0e46583cc
fabricesumsa2000@gmail.com <fabrice.sumsa@epitech.eu>
2021-11-26 17:11:54 +0100

    add layout grid and customcard

--- app/recommender-front/src/components/Shop/index.js
+++ app/recommender-front/src/components/Shop/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
+import CustomCard from "../Utils/CustomCard";
+import StackGrid from "react-stack-grid";
+
+
 
 const Shop = () => {
-  return <div>SHOP</div>;
+  return (
+    <>
+    <StackGrid
+        //columnWidth={width <= 768 ? '100%' : '33.33%'}
+        columnWidth={'33.33%'}
+        gutterWidth={15}
+        gutterHeight={15}
+      >
+        <div key="key1"><CustomCard /></div>
+        <div key="key2"><CustomCard /></div>
+        <div key="key3"><CustomCard >
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          hdhvaahzvdahzvdhazvdhzvada
+          </CustomCard></div>
+        <div key="key4"><CustomCard /></div>
+        <div key="key5"><CustomCard /></div>
+        <div key="key6"><CustomCard /></div>
+    </StackGrid>
+    </>
+  );
 };
 
 export default Shop;
---
+++ app/recommender-front/src/components/Utils/CustomCard.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import styled from 'styled-components'
+
+const StyledDiv =  styled(Card)`
+    /*width: 200px;
+    height: 200px;*/
+`
+
+const CustomCard = props => {
+    return (
+        <div>
+            <StyledDiv sx={{ display: 'flex' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">
+                    Nbre de clients : 150
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                    Nbre de clients : 150
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">
+                    {props.children}
+                </Typography>
+                </CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                </Box>
+            </Box>
+            </StyledDiv>
+        </div>
+    )
+}
+
+CustomCard.propTypes = {
+
+}
+
+export default CustomCard
