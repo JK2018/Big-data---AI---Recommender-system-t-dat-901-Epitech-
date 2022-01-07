eeb39361fea41314571cac2bc0528f86cd6e5df1
Alex <commeau.aws@gmail.com>
2022-01-07 14:21:05 +0100

    server with init enpoint

--- server/run.py
+++ server/run.py
@@ -1,27 +1,42 @@
 
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 from client import *
+from database import *
 import pymongo
-
-app = Flask(__name__)
 
 client = pymongo.MongoClient(
     "mongodb+srv://dat:dat@clusterdat.s2ggv.mongodb.net")
 db = client['datdb']
 dataset = db['storeStats']
 
+# mydoc = dataset.find({})
 
-mydoc = dataset.find({})
+app = Flask(__name__)
+CORS(app)
 
+if client is None:
+    print("pas de client")
 
 @app.route('/')
-def daccord():
-    if client is None:
-        print("ta mere la pute")
-    if client:
-        for document in mydoc:
-            print(document)
-    return 'caca'
+def index():
+    return '/'
+
+
+@app.route('/getSellsInformations')
+def getSellsInformations():
+    info = {}
+    mydoc = dataset.find({})
+    for post in mydoc:
+        if (str(post["_id"]) == "61a176352e90743f7b15c072"):
+            info["totalNbClients"] = post["totalNbClients"]
+            info["totalSales"] = post["totalSales"]
+            info["avgTicketPrice"] = post["avgTicketPrice"]
+            info["avgQtyItemsPerTicket"] = post["avgQtyItemsPerTicket"]
+    return jsonify(
+        info
+    )
 
 
 if __name__ == '__main__':
